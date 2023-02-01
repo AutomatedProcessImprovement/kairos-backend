bb5f4291d1c3f2e54f1a7a14b02fcbb3e0ec75e8
lana <lanakona.lb@gmail.com>
2023-02-01 17:22:23 +0200

    save files in directory

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, Response, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import gridfs
-import string
 import json
 from flask_caching import Cache
-
+import datetime
+import pandas as pd
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
@@ -17,11 +18,12 @@ cache = Cache(app)
 
 client = MongoClient('localhost', 27017)
 db = client.flask_db
-fs = gridfs.GridFS(db)
+files = db.files
 cases = db.cases
 kpi = None
 
 ALLOWED_EXTENSIONS = ['csv', 'xes']
+PATH_TO_LOGS = './logs/'
 
 def initial():
     global kpi
@@ -36,6 +38,12 @@ def initial():
                 print(f'Case {case["_id"]} already exists')
                 continue
 
+def save_file(file):
+    filename = secure_filename(file.filename)
+    path_to_file = str(datetime.datetime.now().timestamp() * 1000) + "_" + filename
+    file.save(os.path.join(PATH_TO_LOGS, path_to_file))
+    return path_to_file
+
 initial()
 
 @app.route('/')
@@ -47,51 +55,60 @@ def upload_file():
     if 'file' not in request.files:
         return Response('No file found',400)
     file = request.files['file']
-    if file and allowed_file(file.filename):
-        file_id = fs.put(file, delimiter = request.form.get('delimiter'), filename = file.filename)
-        return jsonify(fileId = str(file_id))
-    return Response('Incorrect file extension',400)
+    if not file and not allowed_file(file.filename):
+        return Response('Incorrect file extension',400)
+    path_to_file = save_file(file)
+    file_id = files.insert_one({'path': path_to_file,'delimiter': request.form.get('delimiter'), 'datetime': datetime.datetime.now()}).inserted_id
+    return jsonify(fileId = str(file_id))
 
 @app.route('/parse/<file_id>', methods=['PUT'])
 def parse_file(file_id):
-    file = fs.get(ObjectId(file_id))
-    delimiter = file.delimiter
-    if not file:
+    try:
+        file = files.find_one({"_id": ObjectId(file_id)})
+        df = pd.read_csv(PATH_TO_LOGS + file['path'], sep=file['delimiter'])
+        header = list(df.columns)
+        rows = df.values[:7].tolist()
+        return jsonify(header = header, rows = rows)
+    except:
         return Response(f"File with id {file_id} not found",404)
-    rows = []
-    header = []
-    for i in range(7):
-        if i == 0:
-            header = str(file.readline().strip()).translate(str.maketrans('','','\"\''))
-            header = header[1:]
-            header = header.split(delimiter)
-        else:
-            row = str(file.readline().strip()).translate(str.maketrans('','','\"\''))
-            row = row[1:]
-            row = row.split(delimiter)
-            rows.append([row])
-    return jsonify(header = header, rows = list(rows))
 
 @app.route('/update/<file_id>', methods=['POST'])
 def update_types(file_id):
-    file = fs.get(ObjectId(file_id))
-    if not file:
-        return Response(f"File with id {file_id} not found",404)
     types = request.form.get('types')
-    date_format = request.form.get('dateFormat')
-    # TODO send file to zhaosi
+    try:
+        files.find_one_and_update({"_id": ObjectId(file_id)},{"$set":{"types": types}})
+    except:
+        return Response(f'File with {file_id} not found',404)
+    # TODO send file and types to zhaosi
     return Response('Types updated successfully',200)
+
+@app.route('/parameters/<file_id>', methods=['POST'])
+def parameters(file_id):
+    positive_outcome = request.form.get('positive_outcome')
+    treatment = request.form.get('treatment')
+    try:
+        files.find_one_and_update({"_id": ObjectId(file_id)},
+                                    {"$set":{
+                                        "positiveOutcome": positive_outcome,
+                                        "treatment": treatment}})
+    except:
+        return Response(f"File with id {file_id} not found",404)
+
+    # TODO send parameters to zhaosi
+    return Response('Parameters saved successfully',200)
 
 @app.route('/eventlogs')
 def get_eventlogs():
-    files = fs.find({})
-    logs = [{"_id": str(log._id),'filename': log.filename,'uploadDate' : log.uploadDate} for log in files]
+    fs = files.find({})
+    # [print(l) for l in f]
+    logs = [{"_id": str(log["_id"]),'filename': log["path"].split('_',1)[1], 'uploadDate': log['datetime']} for log in fs]
+    # print(list(fs))
     return jsonify(eventlogs = logs)
 
 @app.route('/eventlogs/<file_id>')
 def get_eventlog(file_id):
-    log = fs.get(ObjectId(file_id))
-    e = {"_id": str(log._id),'filename': log.filename,'uploadDate' : log.uploadDate}
+    log = files.find_one({"_id": ObjectId(file_id)})
+    e = {"_id": str(log['_id']),'filename': log["path"].split('_',1)[1],'uploadDate' : log['datetime']}
     return jsonify(eventlog = e)
 
 @app.route('/cases')
@@ -107,15 +124,6 @@ def get_case(case_id):
     c = [obj for obj in list(cases.find({})) if str(obj["_id"]) == str(case_id)][0]
     return jsonify(kpi = kpi, case = c)
 
-@app.route('/parameters/<file_id>', methods=['POST'])
-def parameters(file_id):
-    file = fs.get(ObjectId(file_id))
-    if not file:
-        return Response(f"File with id {file_id} not found",404)
-    positive_outcome = request.form.get('positive_outcome')
-    treatment = request.form.get('treatment')
-    # TODO send parameters to zhaosi
-    return Response('Parameters saved successfully',200)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
