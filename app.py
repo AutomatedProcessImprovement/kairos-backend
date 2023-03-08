cf0cd7207619cbb7de59029aa7cb7bac1fef07ba
Lana Botchorishvili <lanakona.lb@gmail.com>
2023-03-08 22:58:32 +0200

    mongo_uri final fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from kairos.factory import create_app
 
 app = create_app()
-app.config.from_object('config')
+app.config.from_object('config.Config')
 
 # app.run() # only for development (will run app on :5000, which interferes with docker container)
--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-import os
 
 class Config:
     """Base config."""
     PRCORE_HEADERS = {'Authorization':'Bearer UaJW0QvkMA1cVnOXB89E0NbLf3JRRoHwv2wWmaY5v=QYpaxr1UD9/FupeZ85sa2r'}
     PRCORE_BASE_URL = 'https://prcore.chaos.run'
-    MONGO_URI = 'mongodb://localhost:27017/flask_db'
+    MONGO_URI = 'mongodb://mongodb:27017/flask_db' # HOST NAME SHOULD BE THE SAME AS MONGO CONTAINER NAME
     DEBUG = False
