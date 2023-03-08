d5291dc3d2851627ff4c7112d7bec5932835519b
lana <lanakona.lb@gmail.com>
2023-03-08 17:46:00 +0200

    remove some packages, change mongo url

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     """Base config."""
     PRCORE_HEADERS = {'Authorization':'Bearer UaJW0QvkMA1cVnOXB89E0NbLf3JRRoHwv2wWmaY5v=QYpaxr1UD9/FupeZ85sa2r'}
     PRCORE_BASE_URL = 'https://prcore.chaos.run'
-    MONGO_URI = 'mongodb://mongodb:27017/flask_db'
+    MONGO_URI = 'mongodb://mongo:27017/flask_db'
     DEBUG = False
