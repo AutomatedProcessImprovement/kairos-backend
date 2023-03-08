fbb89c460b69e692c8e79427fe47fb538969c61e
lana <lanakona.lb@gmail.com>
2023-03-08 14:55:13 +0200

    delete app.run, configure config.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from kairos.factory import create_app
 app = create_app()
 app.config.from_object('config')
 
-app.run()
+# app.run() # only for development (will run app on :5000, which interferes with docker container)
