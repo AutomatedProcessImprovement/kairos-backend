67c9489692e91b06af750610e906d49e8a1829ce
Lana Botchorishvili <lanakona.lb@gmail.com>
2023-03-08 22:38:51 +0200

    Change config location

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from kairos.factory import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    app.config.from_pyfile('config.py')
+    app.config.from_object('config.Config')
 
     app.run()
