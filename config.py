e9ddb3a58a30359633e2d73c3eeca3768096d718
lana <lanakona.lb@gmail.com>
2023-03-17 02:14:21 +0200

    change mongodb url

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config:
     """Base config."""
     PRCORE_HEADERS = {'Authorization':'Bearer UaJW0QvkMA1cVnOXB89E0NbLf3JRRoHwv2wWmaY5v=QYpaxr1UD9/FupeZ85sa2r'}
     PRCORE_BASE_URL = 'https://prcore.chaos.run'
-    MONGO_URI = 'mongodb://mongodb:27017/flask_db' # HOST NAME SHOULD BE THE SAME AS MONGO CONTAINER NAME
+    MONGO_URI = 'mongodb://db:27017/flask_db' # HOST NAME SHOULD BE THE SAME AS MONGO CONTAINER NAME
     DEBUG = False
