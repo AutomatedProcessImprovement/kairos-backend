4d6e369cb40c7577c5505edb9c28f3221eb62471
lana <lanakona.lb@gmail.com>
2023-03-29 16:52:54 +0300

    add redis

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 
 class Config:
     DEBUG = True
-    MONGO_URI = 'mongodb://localhost:27017'
+    MONGO_URI = 'mongodb://db:27017'
+    REDIS_URL = 'redis://redis:6379'
     PRCORE_HEADERS = {'Authorization':'Bearer UaJW0QvkMA1cVnOXB89E0NbLf3JRRoHwv2wWmaY5v=QYpaxr1UD9/FupeZ85sa2r'}
     PRCORE_BASE_URL = 'https://prcore.chaos.run'
