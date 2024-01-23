c5c0560567202a3d09a3c20744179932cf724934
lana <lanakona.lb@gmail.com>
2024-01-23 17:49:44 +0200

    switch from .config to .env

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from kairos.factory import create_app
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = create_app()
-app.config.from_object('config.Config')
+app.config["PRCORE_HEADERS"] = {"Authorization": os.getenv("PRCORE_TOKEN")}
+app.config["PRCORE_BASE_URL"] = os.getenv("PRCORE_BASE_URL")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
